661391b6c8640999b2c4305482fda52d9078d040
magnusenevoldsen <magnus_enevoldsen@live.dk>
2017-11-03 12:07:07 +0100

    Printet board

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -1,4 +1,51 @@
 
 public class ChessBoard {
-    //Dette er en test. Herpes er en myte.
+
+    String[][] board;
+    String emptyField = " #";
+    String king = " K"; //Konge
+    String queen = " Q"; //Dronning
+    String pawn = " P"; //Bonde
+    String bishop = " B"; //Løber
+    String knight = " Kn"; //Springer
+    String rook = " R"; //Tårn
+
+    public static void main(String[] args) {
+        new ChessBoard();
+    }
+
+    public ChessBoard() {
+        buildBoard();
+        printBoard();
+
+    }
+
+    public void buildBoard() {
+        board = new String[8][8];
+        for (int i = 0; i<8; i++){
+            for (int j = 0; j<8; j++){
+                board[i][j] = emptyField;
+            }
+        }
+        //Placer brikker her:
+        board[1][2] = king;
+    }
+
+
+    public void printBoard() {
+        for(int i = 0; i<8; i++) {
+            for (int j = 0; j<8; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+
+
+
+
 }
